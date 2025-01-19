20defd3a735c89f62feefd15f50de90dbfa688d4
Sri Pravan Paturi <pravanpaturi@gmail.com>
2025-01-19 22:10:16 +0530

    Add RSS feed

--- _config.ts
+++ _config.ts
@@ -3,6 +3,7 @@ import code_highlight from "lume/plugins/code_highlight.ts";
 import date from "lume/plugins/date.ts";
 import sass from "lume/plugins/sass.ts";
 import minifyHTML from "lume/plugins/minify_html.ts";
+import feed from "lume/plugins/feed.ts";
 
 const site = lume({
   src: "./src"
@@ -12,6 +13,18 @@ site.use(code_highlight());
 site.use(date());
 site.use(sass());
 site.use(minifyHTML());
+site.use(feed({
+  output: ["/writings.rss"],
+  query: "type=article|story|poem",
+  info: {
+    title: "=site.title",
+    description: "=site.description",
+  },
+  items: {
+    title: "=title",
+    description: "=excerpt",
+  },
+}));
 
 site.copy("assets", ".");
 
